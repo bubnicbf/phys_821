cbd372136839b0c7bde886ded43e1c2757c6f4d8
Ben Bubnick <bfbubnic@us.ibm.com>
2019-08-23 10:50:42 -0400

    setup still pending

--- star_formation/setup.py
+++ star_formation/setup.py
@@ -2,7 +2,9 @@
 from __future__ import print_function
 import urllib2
 import os
-
+from StringIO import StringIO
+import sys
+import tarfile
 
 
 
@@ -10,10 +12,10 @@ def main(quiet=False):
     '''Pulling all necessary files for using the starformation script
 
 '''
-    print(quiet)
     if quiet:
-        def print(*args):
-            pass
+        output_stream = StringIO()
+    else:
+        output_stream = sys.stdout
 
     newpath = r'%s/models' % os.getcwdu()
     if not os.path.exists(newpath): os.makedirs(newpath)
@@ -32,7 +34,8 @@ def main(quiet=False):
             'http://www.mpia-hd.mpg.de/~robitaille/share/andreas/I4.fits',
             'http://www.mpia-hd.mpg.de/~robitaille/share/andreas/M1.fits',
             'http://www.mpia-hd.mpg.de/~robitaille/share/andreas/M2.fits',
-            'http://www.mpia-hd.mpg.de/~robitaille/share/andreas/M3.fits'
+            'http://www.mpia-hd.mpg.de/~robitaille/share/andreas/M3.fits',
+            ''
           ]
     file_names = [
             'models/parameters.fits.gz',
@@ -45,31 +48,18 @@ def main(quiet=False):
             'models/I4.fits',
             'models/M1.fits',
             'models/M2.fits',
-            'models/M3.fits']
+            'models/M3.fits',
+            'models/extinction_law.tar.gz']
 
     for i in range(len(urls)):
-        print('blub')
         if not os.path.isfile(file_names[i]):
-            u = urllib2.urlopen(urls[i])
             f = open(file_names[i], 'wb')
-            meta = u.info()
-            file_size = int(meta.getheaders("Content-Length")[0])
-            print ("Downloading: %s Bytes: %s" % (file_names[i], file_size))
-
-            file_size_dl = 0
-            block_sz = 8192
-            while True:
-                buffer = u.read(block_sz)
-                if not buffer:
-                    break
-
-                file_size_dl += len(buffer)
-                f.write(buffer)
-                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-                status = status + chr(8)*(len(status)+1)
-                print (status)
-
+            f.write(urllib2.urlopen(urls[i]).read())
             f.close()
+            print('Downloaded %s from %s' % (file_names[i],urls[i]), file=output_stream)
 
-    
-    
+
+    f = tarfile.open(path, 'r:gz')
+    try: f.extractall()
+    finally: f.close()
+
